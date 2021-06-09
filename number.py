36549279289d0278fb91f7fc245d72bbaa8686cf
splaiser <76589075+splaiser@users.noreply.github.com>
2021-06-09 12:15:05 +0300

    numbers

---
+++ number.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5,6,7,8,9]
+for n in numbers:
+    if n == 1:
+        print(f"1st")
+    elif n == 2:
+        print(f"2nd")
+    elif n == 3:
+        print(f"3rd")
+    else:
+        print(f'{n}th')
