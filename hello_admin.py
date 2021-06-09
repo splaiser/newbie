c4bce99c2abde17f4e23c32f931b3d88689a20bb
splaiser <76589075+splaiser@users.noreply.github.com>
2021-06-09 12:02:47 +0300

    start

--- hello_admin.py
+++ hello_admin.py
@@ -1,10 +1,11 @@
-users = []
-if users:
-	for user in users:
-		if user == 'admin':
-			print (f'Hello Master {user}. Want to see report? ')	
+current_users = ['admin','user1','user2','user3','user4']
+new_users = ['User1','user2','user6','user7','user8']
+if new_users:
+	for new_user in new_users:
+		if new_user in current_users :
+			print (f'Hello Master {new_user}. You are log in. ')
 		else :
-			print (f"Hello Dear friend {user}, how are you?")
+			print (f"Hello, try to change login and try again")
 else:
 	print (f"We are need user to auth")			
 
