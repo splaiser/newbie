30027b69d7c3c86a14ebae218196c47834f96feb
splaiser <76589075+splaiser@users.noreply.github.com>
2021-06-09 22:02:42 +0300

    numbers

---
+++ People.py
@@ -0,0 +1,8 @@
+people_0 = {'first_name' : 'nikita', 'last_name' : 'chernyavskiy', 'age' : '29' , 'city' : 'rostov-on-don'}
+print(people_0)
+print(people_0['first_name'])
+print(people_0['last_name'])
+print(people_0['age'])
+print(people_0['city'])
+
+
