9f145833aa1bd8a73fedc3549f21b6ac670225bd
splaiser <76589075+splaiser@users.noreply.github.com>
2021-06-09 11:50:59 +0300

    Initial commit

---
+++ hello_admin.py
@@ -0,0 +1,11 @@
+users = []
+if users:
+	for user in users:
+		if user == 'admin':
+			print (f'Hello Master {user}. Want to see report? ')	
+		else :
+			print (f"Hello Dear friend {user}, how are you?")
+else:
+	print (f"We are need user to auth")			
+
+
