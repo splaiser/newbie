05a69dc0f8d7dac95310726b05eae826019cf8f9
splaiser <76589075+splaiser@users.noreply.github.com>
2021-06-10 13:58:46 +0300

    numbers

---
+++ dice.py
@@ -0,0 +1,10 @@
+lucky_number = {'nikita' : 13, 'sveta' : 8, 'valya' : 3, 'gena' : 7 }
+
+favorite_number = lucky_number['nikita']
+print(f"Nikitas favorite lucky number is {favorite_number}")
+favorite_number = lucky_number['sveta']
+print(f"Svetas favorite lucky number is {favorite_number}")
+favorite_number = lucky_number['valya']
+print(f"valyas favorite lucky number is {favorite_number}")
+favorite_number = lucky_number['gena']
+print(f"Genas favorite lucky number is {favorite_number}")
---
+++ glossary.py
@@ -0,0 +1,17 @@
+glossary = {
+    'for' : 'перебирает значения',
+    'if' : 'условие - если',
+    'terminal' : 'место где записывается и обрабатываются команды',
+    'vocabulary' : 'место хранения множества ключей и их значений',
+    'list' : 'список значений'
+}
+value = glossary['for']
+print(f"for - {value}")
+value = glossary['if']
+print(f"if это - {value}")
+value = glossary['terminal']
+print(f"terminal это - {value}")
+value = glossary['vocabulary']
+print(f"vocabulary это - {value}")
+alue = glossary['list']
+print(f"list это - {value}")
