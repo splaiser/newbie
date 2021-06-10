ee08e60057b0dc861b9aa8f8eb06851fb82f6d98
splaiser <76589075+splaiser@users.noreply.github.com>
2021-06-10 14:11:12 +0300

    glossary

--- glossary.py
+++ glossary.py
@@ -1,17 +1,25 @@
 glossary = {
     'for' : 'перебирает значения',
-    'if' : 'условие - если',
+    'if' : 'условие выполнения функции - если',
     'terminal' : 'место где записывается и обрабатываются команды',
     'vocabulary' : 'место хранения множества ключей и их значений',
-    'list' : 'список значений'
+    'list' : 'список изменяемых значений',
+    'key' : 'ключ по которому можно найти значение из словаря',
+    'value' : 'значение которое связанно с ключем в словаре',
+    'while' : 'условие выполнение функции - пока',
+    'cortege' : 'список неизменных значений',
+    'python' : 'язык программирования'
 }
-value = glossary['for']
-print(f"for - {value}")
-value = glossary['if']
-print(f"if это - {value}")
-value = glossary['terminal']
-print(f"terminal это - {value}")
-value = glossary['vocabulary']
-print(f"vocabulary это - {value}")
-alue = glossary['list']
-print(f"list это - {value}")
+for key,value in glossary.items():
+    print(f'{key} - {value}')
+
+#value = glossary['for']
+#print(f"for - {value}")
+#value = glossary['if']
+#print(f"if это - {value}")
+#value = glossary['terminal']
+#print(f"terminal это - {value}")
+#value = glossary['vocabulary']
+#print(f"vocabulary это - {value}")
+#alue = glossary['list']
+#print(f"list это - {value}")
