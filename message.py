56a4461e08b731af59440e99c1108418b2ddec26
splaiser <76589075+splaiser@users.noreply.github.com>
2021-06-11 14:42:33 +0300

    Function

--- message.py
+++ message.py
@@ -2,5 +2,20 @@ def show_message(texts):
     for text in texts:
         msg = f"Text your message is : {text} "
         print(msg)
+def send_messages(texts):
+    while texts:
+        any_text = texts.pop()
+        sent_messages.append(any_text)
+
+
+
+
+
 text_messages = ['Yes this a text','Yes this is the text','No this is not a text']
+sent_messages = []
 show_message(text_messages)
+send_messages(text_messages[:])
+print(sent_messages)
+print(text_messages)
+
+
