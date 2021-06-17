fbb90acdde3092b3c91cdf7126db4b09a2cb366b
splaiser <76589075+splaiser@users.noreply.github.com>
2021-06-17 23:39:22 +0300

    tre-except

--- File/examples/search.py
+++ File/examples/search.py
@@ -1,10 +1,13 @@
 def cats_and_dogs(filename):
+    #при перемещении текстовых файлов с влюченным except выводиться сообщение о том, что файл недоступен.
+    #При выключенным except файл просто пропускается, а функция продолжает работу
     try:
         with open(filename) as f:
             contents = f.read()
 
     except FileNotFoundError:
-        print("File not found!")
+        #print("File not found!")
+        pass
     else:
         print(contents)
 
