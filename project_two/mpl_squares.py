c3a3b88e916525d780d080575ab6d742b5baea27
splaiser <76589075+splaiser@users.noreply.github.com>
2021-07-07 18:16:30 +0300

    project two ver 0.1

--- project_two/mpl_squares.py
+++ project_two/mpl_squares.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1,1001))
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c='red', s=10)
 #ax.plot(input_values, squares, linewidth=3)
 
 ax.set_title("Square Numbers", fontsize=24)
@@ -15,5 +13,7 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 ax.tick_params(axis='both', labelsize=14)
+
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
