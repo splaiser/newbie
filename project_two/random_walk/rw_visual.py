d75a7070985a8dd1a64ec05865ac95e75d2b09f3
splaiser <76589075+splaiser@users.noreply.github.com>
2021-07-07 19:44:55 +0300

    project two RW ver 0.1

---
+++ project_two/random_walk/rw_visual.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+while True:
+
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+
+    plt.show()
+    keep_running = input("Make another walk? (y/n)")
+    if keep_running == 'n':
+        break
